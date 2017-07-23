da50c8627e19b82b81ac2800fe639aca9da17c81
root <root@mk1.suse>
2017-07-23 01:37:24 -0300

    Pesos funcionando com SharePreferences

--- app/src/main/java/br/univali/calculadorademeediasunivali/MainActivity.java
+++ app/src/main/java/br/univali/calculadorademeediasunivali/MainActivity.java
@@ -2,9 +2,9 @@ package br.univali.calculadorademeediasunivali;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,7 +13,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,12 +28,16 @@ public class MainActivity extends AppCompatActivity {
     private EditText EPr2M3;
     private TextView EM3;
 
-    private int PP1M1 = 1;
-    private int PP2M1 = 1;
-    private int PP1M2 = 1;
-    private int PP2M2 = 1;
-    private int PP1M3 = 1;
-    private int PP2M3 = 1;
+    private int PP1M1;
+    private int PP2M1;
+    private int PP1M2;
+    private int PP2M2;
+    private int PP1M3;
+    private int PP2M3;
+
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,46 +46,60 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //DEFININDO SHAREDPREFERENCES COM OS VALORES DEFAULT
+        sharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        editor.putInt("P-PP1M1", 1);
+        editor.putInt("P-PP2M1", 1);
+        editor.putInt("P-PP1M2", 1);
+        editor.putInt("P-PP2M2", 1);
+        editor.putInt("P-PP1M3", 1);
+        editor.putInt("P-PP2M3", 1);
+        editor.apply();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabCalcular);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //PEGANDO OS VALORES SALVOS EM SHAREDPREFERENCES, CASO ESTEJA VAZIO SETADO '1'
+                sharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+                PP1M1 = sharedPref.getInt("P-PP1M1", 1);
+                PP2M1 = sharedPref.getInt("P-PP2M1", 1);
+                PP1M2 = sharedPref.getInt("P-PP1M2", 1);
+                PP2M2 = sharedPref.getInt("P-PP2M2", 1);
+                PP1M3 = sharedPref.getInt("P-PP1M3", 1);
+                PP2M3 = sharedPref.getInt("P-PP2M3", 1);
+
                 EPr1M1 = (EditText) findViewById(R.id.editTextPr1M1);
                 EPr2M1 = (EditText) findViewById(R.id.editTextPr2M1);
-//                EditText EM1 = (EditText) findViewById(R.id.editTextM1);
                 EM1 = (TextView) findViewById(R.id.editTextM1);
                 float Pr1M1 = new Float(EPr1M1.getText().toString()).floatValue();
                 float Pr2M1 = new Float(EPr2M1.getText().toString()).floatValue();
-                float M1 = new Float(((Pr1M1*PP1M1)+(Pr2M1*PP2M1))/(PP1M1+PP2M1));
-                EM1.setText(String.valueOf(M1));
+                float M1 = new Float(((Pr1M1*PP1M1)+(Pr2M1*PP2M1))/(PP1M1+PP2M1));  //REALIZANDO CALCULO DA 'M' JÁ COM OS PESOS
+                EM1.setText(String.format("%.2f",M1));                              //SETANDO O VALOR DA 'M' COM APENAS 2 CASAS DECIMAIS
                 // -- //
                 EPr1M2 = (EditText) findViewById(R.id.editTextPr1M2);
                 EPr2M2 = (EditText) findViewById(R.id.editTextPr2M2);
-//                EditText EM2 = (EditText) findViewById(R.id.editTextM2);
                 EM2 = (TextView) findViewById(R.id.editTextM2);
                 float Pr1M2 = new Float(EPr1M2.getText().toString()).floatValue();
                 float Pr2M2 = new Float(EPr2M2.getText().toString()).floatValue();
-                float M2 = new Float(((Pr1M2*PP1M2)+(Pr2M2*PP2M2))/(PP1M2+PP2M2));
-                EM2.setText(String.valueOf(M2));
+                float M2 = new Float(((Pr1M2*PP1M2)+(Pr2M2*PP2M2))/(PP1M2+PP2M2));  //REALIZANDO CALCULO DA 'M' JÁ COM OS PESOS
+                EM2.setText(String.format("%.2f",M2));                              //SETANDO O VALOR DA 'M' COM APENAS 2 CASAS DECIMAIS
                 // -- //
                 EPr1M3 = (EditText) findViewById(R.id.editTextPr1M3);
                 EPr2M3 = (EditText) findViewById(R.id.editTextPr2M3);
-//                EditText EM3 = (EditText) findViewById(R.id.editTextM3);
                 EM3 = (TextView) findViewById(R.id.editTextM3);
                 float Pr1M3 = new Float(EPr1M3.getText().toString()).floatValue();
                 float Pr2M3 = new Float(EPr2M3.getText().toString()).floatValue();
-                float M3 = new Float(((Pr1M3*PP1M3)+(Pr2M3*PP2M3))/(PP1M3+PP2M3));
-                EM3.setText(String.valueOf(M3));
+                float M3 = new Float(((Pr1M3*PP1M3)+(Pr2M3*PP2M3))/(PP1M3+PP2M3));  //REALIZANDO CALCULO DA 'M' JÁ COM OS PESOS
+                EM3.setText(String.format("%.2f",M3));                              //SETANDO O VALOR DA 'M' COM APENAS 2 CASAS DECIMAIS
                 // -- //
                 float MF = ((M1 + M2 + M3)/3);
                 if (MF > 5.75) {
-//                    Toast.makeText(MainActivity.this,Float.toString(MF) + ", você foi aprovado",Toast.LENGTH_SHORT).show();
-//                    teste("Você foi aprovado");
                     show(MainActivity.this, "Média", "Você foi aprovado", android.R.drawable.ic_dialog_info);
                 } else {
-//                    Toast.makeText(MainActivity.this,Float.toString(MF) + ", você foi reprovado",Toast.LENGTH_SHORT).show();
-//                    show(MainActivity.this, "Média", "Você foi aprovado", android.R.drawable.ic_dialog_info);
-                    teste("Você foi reprovado");
+                    show(MainActivity.this, "Média", "Você foi aprovado", android.R.drawable.ic_dialog_info);
                 }
 
             }
@@ -106,9 +123,13 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-//            moveTaskToBack(true);
-//            android.os.Process.killProcess(android.os.Process.myPid());
-//            System.exit(1);
+            /*
+                DESTROI O APP NOS PROCESSOS DO SISTEMA OPERACIONAL
+
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            */
             finish();
         }
 
@@ -123,11 +144,13 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     public void sendMessage(View view) {
 
 
     }
 
+    //METODO PARA LIMPAR OS CAMPOS
     private void clearLabel(){
         EPr1M1 = (EditText) findViewById(R.id.editTextPr1M1);
         EPr2M1 = (EditText) findViewById(R.id.editTextPr2M1);
@@ -151,12 +174,8 @@ public class MainActivity extends AppCompatActivity {
         EM3.setText(String.valueOf(""));
     }
 
-    private void teste(String status){
-        show(this, "Média", status, android.R.drawable.ic_dialog_info);
-    }
-
+    //METODO PARA EMITIR ALERT NA TELA AO VERIFICAR AS NOTAS
     private void show(Context ctx, String title, String msg, int iconId){
-        //getResources().getDrawable(R.mipmap.ic_launcher);  RECUPERAR IMAGEM DA PASTA MIPMAP
         AlertDialog.Builder dlg = new AlertDialog.Builder(ctx);
         dlg.setIcon(iconId);
         dlg.setTitle(title);
@@ -164,4 +183,5 @@ public class MainActivity extends AppCompatActivity {
         dlg.setNeutralButton("OK",null);
         dlg.show();
     }
+
 }
--- app/src/main/java/br/univali/calculadorademeediasunivali/configActivity.java
+++ app/src/main/java/br/univali/calculadorademeediasunivali/configActivity.java
@@ -1,6 +1,7 @@
 package br.univali.calculadorademeediasunivali;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,11 +16,15 @@ public class configActivity extends AppCompatActivity {
     private EditText edtPP1M3;
     private EditText edtPP2M3;
 
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
 
+        //RESGATANDO COMPONENTES DA TELA PELO ID
         edtPP1M1 = (EditText) findViewById(R.id.ppr1m1);
         edtPP2M1 = (EditText) findViewById(R.id.ppr2m1);
         edtPP1M2 = (EditText) findViewById(R.id.ppr1m2);
@@ -27,13 +32,16 @@ public class configActivity extends AppCompatActivity {
         edtPP1M3 = (EditText) findViewById(R.id.ppr1m3);
         edtPP2M3 = (EditText) findViewById(R.id.ppr2m3);
 
+        //PEGANDO O SHAREDPREFERENCES PARA RESGATAR OS VALORES
+        sharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
 
-        edtPP1M1.setText(String.valueOf(R.string.pp1m1));
-        edtPP2M1.setText(String.valueOf(R.string.pp2m1));
-        edtPP1M2.setText(String.valueOf(R.string.pp1m2));
-        edtPP2M2.setText(String.valueOf(R.string.pp2m2));
-        edtPP1M3.setText(String.valueOf(R.string.pp1m3));
-        edtPP2M3.setText(String.valueOf(R.string.pp2m3));
+        //SETANDO OS VALORES QUE ESTÃO NO SHAREDPREFERENCES NOS ELEMENTOS EDITTEXT DO LAYOUT
+        edtPP1M1.setText(String.valueOf(sharedPref.getInt("P-PP1M1", 1)));
+        edtPP2M1.setText(String.valueOf(sharedPref.getInt("P-PP2M1", 1)));
+        edtPP1M2.setText(String.valueOf(sharedPref.getInt("P-PP1M2", 1)));
+        edtPP2M2.setText(String.valueOf(sharedPref.getInt("P-PP2M2", 1)));
+        edtPP1M3.setText(String.valueOf(sharedPref.getInt("P-PP1M3", 1)));
+        edtPP2M3.setText(String.valueOf(sharedPref.getInt("P-PP2M3", 1)));
 
         Button btnDefinir = (Button) findViewById(R.id.btnDefinir);
 
@@ -41,16 +49,17 @@ public class configActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-//                MainActivity.PP1M1 = Integer.valueOf(edtPP1M1.getText().toString());
-//                MainActivity.PP2M1 = Integer.valueOf(edtPP2M1.getText().toString());
-//                MainActivity.PP1M2 = Integer.valueOf(edtPP1M2.getText().toString());
-//                MainActivity.PP2M2 = Integer.valueOf(edtPP2M2.getText().toString());
-//                MainActivity.PP1M3 = Integer.valueOf(edtPP1M3.getText().toString());
-//                MainActivity.PP2M3 = Integer.valueOf(edtPP2M3.getText().toString());
+            //SETANDO OS NOVOS VALORES NO SHAREPREFERENCES DE ACORDO COM O QUE FOI INSERIDO NOS EDITTEXT DO LAYOUT
+            editor = sharedPref.edit();
+            editor.putInt("P-PP1M1", Integer.valueOf(edtPP1M1.getText().toString()));
+            editor.putInt("P-PP2M1", Integer.valueOf(edtPP2M1.getText().toString()));
+            editor.putInt("P-PP1M2", Integer.valueOf(edtPP1M2.getText().toString()));
+            editor.putInt("P-PP2M2", Integer.valueOf(edtPP2M2.getText().toString()));
+            editor.putInt("P-PP1M3", Integer.valueOf(edtPP1M3.getText().toString()));
+            editor.putInt("P-PP2M3", Integer.valueOf(edtPP2M3.getText().toString()));
+            editor.apply();
 
-                
-
-                configActivity.this.finish();
+            configActivity.this.finish();
 
             }
         });
